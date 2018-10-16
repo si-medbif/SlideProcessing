ec9315c48463242bde5568dd1e3e0583f4932a8d
DumMai <dumrong_m@yahoo.com>
2018-10-16 13:15:24 +0700

    Create Image_chop.py

---
+++ Image_chop.py
@@ -0,0 +1,10 @@
+import openslide
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+
+a = openslide.OpenSlide(filename="/Users/dmairiang/Downloads/AFB_5081A.svs")
+
+b = a.read_region((10000,10000),level= 0 ,size= (300,300))
+
+plt.imshow(b)
