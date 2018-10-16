2223f09143769b7fc62774add7f0887c927e3e2c
DumMai <dumrong_m@yahoo.com>
2018-10-16 13:22:23 +0700

    Update Image_chop.py

--- Image_chop.py
+++ Image_chop.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-a = openslide.OpenSlide(filename="Path/to/file.svs)#("/Users/dmairiang/Downloads/AFB_5081A.svs")
+a = openslide.OpenSlide(filename="Path/to/file.svs")#("/Users/dmairiang/Downloads/AFB_5081A.svs")
 
 b = a.read_region((10000,10000),level= 0 ,size= (300,300))
 
