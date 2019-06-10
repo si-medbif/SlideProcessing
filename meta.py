67e91360215f8f77cda2668f29e10ce5daa96921
DumMai <dumrong_m@yahoo.com>
2019-06-10 10:54:15 +0700

    Update meta.py

--- meta.py
+++ meta.py
@@ -1,5 +1,26 @@
 import os
+import argparse
 
 
-#Read GDC sample sheet
-def readGDC(filename):
+#Read GDC sample sheet (With header)
+def readGDC(filename, header = True):
+    with open(filename, 'r') as f:
+        if header:
+            f.readline()
+        for i in f:
+            print(i)
+
+def main(args):
+    readGDC(args.file_name)
+
+  
+if __name__ == '__main__':
+    
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument(
+        "-f",
+        "--file_name",
+        action="store",
+        help="Name and path of the sample sheet file"
+    )
