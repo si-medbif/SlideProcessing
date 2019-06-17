b6c9438442e35e95fc1eac95f9d0a5a766cd7e40
DumMai <dumrong_m@yahoo.com>
2019-06-17 15:06:06 +0700

    Update meta2.sh

--- meta2.sh
+++ meta2.sh
@@ -1,2 +1,28 @@
 
 singularity run DeepPATHv4.sif python /opt/DeepPATH/DeepPATH_code/00_preprocessing/0d_SortTiles.py --SourceFolder=/home/dumrong/tiles/Results/ --JsonFile=/home/dumrong/Sort_seed1234567.txt --Magnification=20.0 --SortingOption=14 --PercentTest=15 --PercentValid=15 --PatientID=12 --nSplit 0 --MagDiffAllowed=5
+
+
+##Results for SEED = 1234567
+
+#('Benign', 930838)
+#('Benign_test', 114497)
+#('Benign_train', 662724)
+#('Benign_valid', 153617)
+#('CA', 1236102)
+#('CA_test', 193780)
+#('CA_train', 843499)
+#('CA_valid', 198823)
+#('Benign_test', 0.12300421770490676)
+#('Benign_train', 0.7119649176333583)
+#('Benign_valid', 0.16503086466173492)
+#('CA_test', 0.1567669981927058)
+#('CA_train', 0.6823862432064668)
+#('CA_valid', 0.16084675860082745)
+#('Benign', 970)
+#('Benign_test', 162)
+#('Benign_train', 655)
+#('Benign_valid', 153)
+#('CA', 700)
+#('CA_test', 107)
+#('CA_train', 484)
+#('CA_valid', 109)
