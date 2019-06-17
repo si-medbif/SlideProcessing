301c86097a6fa255ed46345953533e6ef88c947b
DumMai <dumrong_m@yahoo.com>
2019-06-17 14:18:08 +0700

    Create meta2.sh

---
+++ meta2.sh
@@ -0,0 +1,2 @@
+
+python /opt/DeepPATH/DeepPATH_code/00_preprocessing/0d_SortTiles.py --SourceFolder=/home/dumrong/tiles/ --JsonFile=/home/dumrong/Sort_seed1234567.txt --Magnification=20 --SortingOption=14 --PercentTest=15 --PercentValid=15 --PatientID=12 --nSplit 0
