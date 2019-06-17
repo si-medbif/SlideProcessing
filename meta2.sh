2dea04b3fe24140ad3e64d889b2de9818cc47ba4
DumMai <dumrong_m@yahoo.com>
2019-06-17 14:31:21 +0700

    Update meta2.sh

--- meta2.sh
+++ meta2.sh
@@ -1,2 +1,2 @@
 
-python /opt/DeepPATH/DeepPATH_code/00_preprocessing/0d_SortTiles.py --SourceFolder=/home/dumrong/tiles/ --JsonFile=/home/dumrong/Sort_seed1234567.txt --Magnification=20 --SortingOption=14 --PercentTest=15 --PercentValid=15 --PatientID=12 --nSplit 0
+singularity run DeepPATHv4.sif python /opt/DeepPATH/DeepPATH_code/00_preprocessing/0d_SortTiles.py --SourceFolder=/home/dumrong/tiles/Results/ --JsonFile=/home/dumrong/Sort_seed1234567.txt --Magnification=20.0 --SortingOption=14 --PercentTest=15 --PercentValid=15 --PatientID=12 --nSplit 0 --MagDiffAllowed=5
